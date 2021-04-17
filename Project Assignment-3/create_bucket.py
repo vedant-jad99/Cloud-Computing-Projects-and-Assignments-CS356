0696eb8db4841bc95c85e3e546344684b05cb67b
Vedant <vedantjadhav5566@gmail.com>
2021-04-17 18:07:02 +0000

    Modified create_bucket file

--- Project Assignment-3/create_bucket.py
+++ Project Assignment-3/create_bucket.py
@@ -1,5 +1,6 @@
 #Creating an s3 bucket in aws
 import boto3 as boto
 
+BUCKET = open("bucket-name.txt").read().strip().lower()
 s3 = boto.resource('s3')
 s3.create_bucket(Bucket="ubunturekbucket1", CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
